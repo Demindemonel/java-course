4ab3fc773ceb14c35ac2dbb2477b41f729415c6b
monate <dmitryklimenkov@gmail.com>
2021-07-04 14:09:18 +0300

    Added processing of the input file.

--- src/main/java/io/main/Main.java
+++ src/main/java/io/main/Main.java
@@ -1,25 +1,66 @@
 package io.main;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
+    private static List<Integer> filenameLength = new ArrayList<>();
+    private static List<Integer> filesInFolder = new ArrayList<>();
+    private static int totalNumberOfFolders;
+    private static int totalNumberOfFiles;
     private static int numberOfFoldersInParentDirectory;
     private static StringBuilder stringBuilder = new StringBuilder();
 
     public static void main(String[] args) {
         File fileOrFolder = new File(args[0]);
-        for (File file : fileOrFolder.listFiles()) {
-            if (file.isDirectory()) numberOfFoldersInParentDirectory++;
-        }
-        stringBuilder.append(fileOrFolder.getName() + "\n");
-        getDirsAndFiles(fileOrFolder, true);
-        try (PrintStream printStream = new PrintStream(new FileOutputStream("data\\output_file.txt"))) {
-            printStream.print(stringBuilder);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        if (fileOrFolder.isDirectory()) {
+            for (File file : Objects.requireNonNull(fileOrFolder.listFiles())) {
+                if (file.isDirectory()) numberOfFoldersInParentDirectory++;
+            }
+            stringBuilder.append(fileOrFolder.getName()).append("\n");
+            getDirsAndFiles(fileOrFolder, true);
+            stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+            try (PrintStream printStream = new PrintStream(new FileOutputStream("data\\output_file.txt"))) {
+                printStream.print(stringBuilder);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try (FileReader fileReader = new FileReader(fileOrFolder);
+                 BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                StringBuilder stringBuffer = new StringBuilder();
+                String line;
+                int filesInFolderCounter = 0;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line);
+                    if (stringBuffer.toString().equals("    |")) {
+                        filesInFolder.add(filesInFolderCounter);
+                        filesInFolderCounter = 0;
+                        stringBuffer.delete(0, stringBuffer.length());
+                        continue;
+                    }
+                    if (stringBuffer.toString().startsWith("    |-----")) {
+                        totalNumberOfFolders++;
+                    } else if (stringBuffer.toString().startsWith("    |       ") || stringBuffer.toString().startsWith("            ")) {
+                        totalNumberOfFiles++;
+                        filenameLength.add(stringBuffer.toString().length());
+                        filesInFolderCounter++;
+                    } else {
+                        totalNumberOfFolders++;
+                        filesInFolder.add(filesInFolderCounter);
+                    }
+                    stringBuffer.delete(0, stringBuffer.length());
+                }
+                filesInFolder.add(filesInFolderCounter);
+                System.out.println(totalNumberOfFolders);
+                System.out.println(totalNumberOfFiles);
+                System.out.println(filenameLength.stream().mapToInt(value -> value).average().getAsDouble());
+                System.out.println(filesInFolder.stream().mapToInt(value -> value).average().getAsDouble());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -32,10 +73,11 @@ public class Main {
                     getDirsAndFiles(file.listFiles()[i], false);
                 } else {
                     if (numberOfFoldersInParentDirectory == 0) {
-                        stringBuilder.append("            " + file.listFiles()[i].getName() + "\n");
+                        stringBuilder.append("            ");
                     } else {
-                        stringBuilder.append("    |       " + file.listFiles()[i].getName() + "\n");
+                        stringBuilder.append("    |       ");
                     }
+                    stringBuilder.append(file.listFiles()[i].getName()).append("\n");
                 }
             }
         }
