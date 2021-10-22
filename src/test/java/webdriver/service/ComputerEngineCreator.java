a1a0d7c4269826dc40842c475a86a1ca8ceb3735
demindemonel <dmitryklimenkov@gmail.com>
2021-10-22 09:32:12 +0300

    TESTDATA parameters were created

--- src/test/java/webdriver/service/ComputerEngineCreator.java
+++ src/test/java/webdriver/service/ComputerEngineCreator.java
@@ -3,17 +3,17 @@ package webdriver.service;
 import webdriver.model.ComputerEngine;
 
 public class ComputerEngineCreator {
-    public static final String TESTDATA_NUMBER_OF_INSTANCES = "testdata.user.name";
-    public static final String TESTDATA_OPERATION_SYSTEM_SOFTWARE = "";
-    public static final String TESTDATA_MACHINE_CLASS = "";
-    public static final String TESTDATA_MACHINE_FAMILY = "";
-    public static final String TESTDATA_SERIES = "";
-    public static final String TESTDATA_MACHINE_TYPE = "";
-    public static final String TESTDATA_NUMBER_OF_GPUS = "";
-    public static final String TESTDATA_GPU_TYPE = "";
-    public static final String TESTDATA_LOCAL_SSD = "";
-    public static final String TESTDATA_DATACENTER_LOCATION = "";
-    public static final String TESTDATA_COMMITTED_USAGE = "";
+    public static final String TESTDATA_NUMBER_OF_INSTANCES = "testdata.computeengine.numberofinstances";
+    public static final String TESTDATA_OPERATION_SYSTEM_SOFTWARE = "testdata.computeengine.operationsystemsoftware";
+    public static final String TESTDATA_MACHINE_CLASS = "testdata.computeengine.machineclass";
+    public static final String TESTDATA_MACHINE_FAMILY = "testdata.computeengine.machinefamily";
+    public static final String TESTDATA_SERIES = "testdata.computeengine.series";
+    public static final String TESTDATA_MACHINE_TYPE = "testdata.computeengine.machinetype";
+    public static final String TESTDATA_NUMBER_OF_GPUS = "testdata.computeengine.numberofgpus";
+    public static final String TESTDATA_GPU_TYPE = "testdata.computeengine.gputype";
+    public static final String TESTDATA_LOCAL_SSD = "testdata.computeengine.localssd";
+    public static final String TESTDATA_DATACENTER_LOCATION = "testdata.computeengine.datacenterlocation";
+    public static final String TESTDATA_COMMITTED_USAGE = "testdata.computeengine.committedusage";
 
     public static ComputerEngine withSettingFromProperty() {
         return new ComputerEngine(TESTDATA_NUMBER_OF_INSTANCES,
